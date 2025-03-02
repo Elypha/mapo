24926deb3f01d4ffd7bc5621f505b6a32f515c4e
Elypha <i@elypha.com>
2025-03-03 02:53:17 +0900

    add repkg

---
+++ scripts/repkg.py
@@ -0,0 +1,41 @@
+import platform
+import re
+import shutil
+import sys
+from pathlib import Path
+
+import httpx
+import orjson
+from rich import progress
+
+from lib.config import MapoConfig, Script
+from lib.helper import client, download_helper, extract, grant, junction_latest, remove_helper, symlink_latest, update_helper_github
+from lib.log import LogLevel, console, log, log_error, print_list, print_heading
+
+
+def update(ipc_progress: dict, config: MapoConfig, task: dict) -> dict:
+    args = {
+        "url": "https://api.github.com/repos/notscuffed/repkg/releases/latest",
+        # RePKG.zip
+        "regex_asset": re.compile(r"^RePKG\.zip$"),
+        # v0.3.2-alpha
+        "regex_version": re.compile(r"(?P<version>[\d.\-\w]+)"),
+    }
+    v0, v1 = update_helper_github(ipc_progress, config, task, args)
+    return {"name": task["name"], "v0": v0, "v1": v1}
+
+
+def upgrade(ipc_progress: dict, config: MapoConfig, task: dict) -> dict:
+    # download
+    dl_file_path = download_helper(ipc_progress, config, task)
+    VERSION_DIR = dl_file_path.parent
+    # install
+    extract(dl_file_path)
+    # file_path = VERSION_DIR.glob("ncmdump*", case_sensitive=False)
+    junction_latest(VERSION_DIR)
+    ipc_progress[task["task_id"]] = (ipc_progress[task["task_id"]][1], ipc_progress[task["task_id"]][1])
+    return {"name": task["name"], "v1": VERSION_DIR.name}
+
+
+# def uninstall(ipc_progress: dict, config: MapoConfig, task: dict) -> dict:
+#     single_uninstall(_p_stats, task_id, script, config, cache)
